1e1654a44d508c887cbc819fd607ad563918653a
Satyam Tiwari <iamsatyampandit@gmail.com>
2026-08-26 21:29:23 +0530

    Program 15

---
+++ program15.py
@@ -0,0 +1,6 @@
+#Python Program using functions with keyword arguments.
+def display(uid,name,marks):
+    print("User ID:", uid)
+    print("Name:", name)
+    print("Marks:", marks)
+display(uid=48,marks=95,name="Satyam")
