5229be385e3ff1072d41eeb8b24b28b0a9a1be18
Satyam Tiwari <iamsatyampandit@gmail.com>
2026-08-26 21:26:56 +0530

    Program 14

---
+++ python14.py
@@ -0,0 +1,6 @@
+#Python Program using functions with required arguments.
+def display(uid,name,marks):
+    print("User ID:", uid)
+    print("Name:", name)
+    print("Marks:", marks)
+display(48,"Satyam","95")
