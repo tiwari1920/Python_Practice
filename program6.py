7fc164d47ef09a958fbbfa32dbf0ab62c6ed1866
Satyam Tiwari <iamsatyampandit@gmail.com>
2026-08-26 21:04:55 +0530

    Program 6

---
+++ program6.py
@@ -0,0 +1,11 @@
+#Python Program using match case.
+number = 2
+match number:
+    case 1:
+        print("The number is 1")
+    case 2:
+        print("The number is 2")
+    case 3:
+        print("The number is 3")
+    case _:
+        print("The number is not 1, 2, or 3")
