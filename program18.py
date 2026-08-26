e7513e48b7061cc08288bed947f5d9a840707c28
Satyam Tiwari <iamsatyampandit@gmail.com>
2026-08-26 21:49:52 +0530

    Program 18

---
+++ program18.py
@@ -0,0 +1,9 @@
+#Python Program using Ord/Chr/Id.
+a = 65
+b = 34
+print("The character for ASCII value", a, "is", chr(a))
+print("The character for ASCII value", b, "is", chr(b))
+print("The ASCII value for character", chr(a), "is", ord(chr(a)))
+print("The ASCII value for character", chr(b), "is", ord(chr(b)))
+print("The id of variable a is", id(a))
+print("The id of variable b is", id(b))
