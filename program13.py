7602c04abb048b239869bdcf8c9c5087eeb8ee8e
Satyam Tiwari <iamsatyampandit@gmail.com>
2026-08-26 21:24:50 +0530

    Program 13

---
+++ program13.py
@@ -0,0 +1,7 @@
+#Python Program using functions with default arguments.
+def display(uid,name="Tiwari",marks=100):
+    print("User ID:", uid)
+    print("Name:", name)
+    print("Marks:", marks)
+display(48)
+display(48,"Satyam","95")
