84af9e8e227de17ba8eee2d196ed8bde4793bcc8
Satyam Tiwari <iamsatyampandit@gmail.com>
2026-08-26 20:59:33 +0530

    Program 4

---
+++ program4.py
@@ -0,0 +1,12 @@
+#Python Program using simple if, if-else
+#if-else:
+n = 7
+if n % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+#simple if:
+age = 20
+if age >= 18:
+    print("You are eligible to vote")
